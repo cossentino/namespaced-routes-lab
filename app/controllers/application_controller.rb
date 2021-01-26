ff47a543c21526c3d3752a1d51fb168fa27d1038
Ian Cossentino <icossentino@gmail.com>
2021-01-26 10:33:06 -0500

    commit tree f614a409fd358fb7c012304da403a1d848963dec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  private
+
+    def preference_on(preference)
+      Preference.first.send(preference) == true
+    end
+
+  
+
 end
